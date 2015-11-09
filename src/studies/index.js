13c6a3371c4a6f58f9af9886c38dfe00bf558784
Chad Johnson <chad.d.johnson@gmail.com>
2015-11-09 14:52:45 -0600

    Starting over.

--- src/studies/index.js
+++
@@ -1,4 +0,0 @@
-module.exports.Ema = require('./Ema');
-module.exports.Sma = require('./Sma');
-module.exports.Rsi = require('./Rsi');
-module.exports.PolynomialRegressionChannel = require('./PolynomialRegressionChannel');
