5e61b8e6b84ad546c638bfbe89e6e13eb021cb3a
Chad Johnson <chad.d.johnson@gmail.com>
2015-11-14 16:57:21 -0600

    Simplified userscript file organization.

--- src/userscripts/CTOption.js
+++ src/userscripts/CTOption.js
@@ -4,7 +4,7 @@ function CTOption() {
     this.constructor = CTOption;
 
     var self = this;
-    var symbols = ['EURGBP', 'AUDNZD', 'NZDUSD', 'AUDCAD', 'USDJPY', 'AUDUSD', 'USDCAD', 'USDCHF', 'EURUSD'];
+    var symbols = ['EURGBP-OTC', 'AUDNZD-OTC', 'NZDUSD-OTC', 'AUDCAD-OTC', 'USDJPY-OTC', 'AUDUSD-OTC', 'USDCAD-OTC', 'USDCHF-OTC', 'EURUSD-OTC'];
 
     Base.call(self, symbols);
 
