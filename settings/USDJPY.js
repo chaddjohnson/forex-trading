3484082388f5d4cc9e9858d5086462972d728f4f
Chad Johnson <chad.d.johnson@gmail.com>
2015-11-20 20:52:58 -0600

    Installed new configuration settings for USDJPY.

--- settings/USDJPY.js
+++ settings/USDJPY.js
@@ -4,43 +4,25 @@ module.exports = {
     configurations: [
         {
             "trendPrChannel": {
-                "regression": "trendPrChannel300_2"
+                "regression": "trendPrChannel700_2"
             },
             "prChannel": {
-                "lower": "prChannelLower200_3_19",
-                "upper": "prChannelUpper200_3_19"
+                "lower": "prChannelLower200_2_20",
+                "upper": "prChannelUpper200_2_20"
             },
             "rsi": {
-                "oversold": 20,
-                "overbought": 80,
-                "rsi": "rsi5"
+                "oversold": 23,
+                "overbought": 77,
+                "rsi": "rsi7"
             },
             "sma13": false,
-            "ema50": false,
-            "ema100": false,
-            "ema200": false
-        },
-        {
-            "trendPrChannel": {
-                "regression": "trendPrChannel400_2"
-            },
-            "prChannel": {
-                "lower": "prChannelLower100_2_21",
-                "upper": "prChannelUpper100_2_21"
-            },
-            "rsi": {
-                "oversold": 20,
-                "overbought": 80,
-                "rsi": "rsi5"
-            },
-            "sma13": true,
             "ema50": true,
             "ema100": true,
             "ema200": true
         },
         {
             "trendPrChannel": {
-                "regression": "trendPrChannel300_2"
+                "regression": "trendPrChannel750_2"
             },
             "prChannel": {
                 "lower": "prChannelLower200_3_19",
@@ -52,85 +34,17 @@ module.exports = {
                 "rsi": "rsi7"
             },
             "sma13": false,
-            "ema50": false,
-            "ema100": false,
-            "ema200": false
-        },
-        {
-            "trendPrChannel": null,
-            "prChannel": {
-                "lower": "prChannelLower300_2_215",
-                "upper": "prChannelUpper300_2_215"
-            },
-            "rsi": {
-                "oversold": 20,
-                "overbought": 80,
-                "rsi": "rsi7"
-            },
-            "sma13": true,
-            "ema50": true,
-            "ema100": false,
-            "ema200": false
-        },
-        {
-            "trendPrChannel": null,
-            "prChannel": {
-                "lower": "prChannelLower300_2_21",
-                "upper": "prChannelUpper300_2_21"
-            },
-            "rsi": {
-                "oversold": 20,
-                "overbought": 80,
-                "rsi": "rsi7"
-            },
-            "sma13": true,
             "ema50": true,
             "ema100": true,
             "ema200": false
         },
         {
             "trendPrChannel": {
-                "regression": "trendPrChannel450_2"
+                "regression": "trendPrChannel700_2"
             },
             "prChannel": {
-                "lower": "prChannelLower200_2_195",
-                "upper": "prChannelUpper200_2_195"
-            },
-            "rsi": {
-                "oversold": 23,
-                "overbought": 77,
-                "rsi": "rsi7"
-            },
-            "sma13": true,
-            "ema50": true,
-            "ema100": false,
-            "ema200": false
-        },
-        {
-            "trendPrChannel": {
-                "regression": "trendPrChannel300_2"
-            },
-            "prChannel": {
-                "lower": "prChannelLower250_4_20",
-                "upper": "prChannelUpper250_4_20"
-            },
-            "rsi": {
-                "oversold": 5,
-                "overbought": 95,
-                "rsi": "rsi2"
-            },
-            "sma13": true,
-            "ema50": true,
-            "ema100": true,
-            "ema200": true
-        },
-        {
-            "trendPrChannel": {
-                "regression": "trendPrChannel300_2"
-            },
-            "prChannel": {
-                "lower": "prChannelLower250_4_19",
-                "upper": "prChannelUpper250_4_19"
+                "lower": "prChannelLower100_3_215",
+                "upper": "prChannelUpper100_3_215"
             },
             "rsi": {
                 "oversold": 20,
@@ -138,43 +52,7 @@ module.exports = {
                 "rsi": "rsi5"
             },
             "sma13": false,
-            "ema50": false,
-            "ema100": false,
-            "ema200": false
-        },
-        {
-            "trendPrChannel": {
-                "regression": "trendPrChannel400_2"
-            },
-            "prChannel": {
-                "lower": "prChannelLower100_2_19",
-                "upper": "prChannelUpper100_2_19"
-            },
-            "rsi": {
-                "oversold": 20,
-                "overbought": 80,
-                "rsi": "rsi7"
-            },
-            "sma13": true,
             "ema50": true,
-            "ema100": true,
-            "ema200": true
-        },
-        {
-            "trendPrChannel": {
-                "regression": "trendPrChannel300_2"
-            },
-            "prChannel": {
-                "lower": "prChannelLower200_3_215",
-                "upper": "prChannelUpper200_3_215"
-            },
-            "rsi": {
-                "oversold": 5,
-                "overbought": 95,
-                "rsi": "rsi2"
-            },
-            "sma13": false,
-            "ema50": false,
             "ema100": true,
             "ema200": true
         },
@@ -187,18 +65,36 @@ module.exports = {
                 "upper": "prChannelUpper200_3_19"
             },
             "rsi": {
-                "oversold": 20,
-                "overbought": 80,
+                "oversold": 23,
+                "overbought": 77,
                 "rsi": "rsi7"
             },
-            "sma13": true,
+            "sma13": false,
             "ema50": true,
             "ema100": true,
             "ema200": false
         },
         {
             "trendPrChannel": {
-                "regression": "trendPrChannel700_2"
+                "regression": "trendPrChannel650_2"
+            },
+            "prChannel": {
+                "lower": "prChannelLower200_3_19",
+                "upper": "prChannelUpper200_3_19"
+            },
+            "rsi": {
+                "oversold": 23,
+                "overbought": 77,
+                "rsi": "rsi7"
+            },
+            "sma13": false,
+            "ema50": false,
+            "ema100": true,
+            "ema200": true
+        },
+        {
+            "trendPrChannel": {
+                "regression": "trendPrChannel650_2"
             },
             "prChannel": {
                 "lower": "prChannelLower200_2_20",
@@ -209,10 +105,136 @@ module.exports = {
                 "overbought": 80,
                 "rsi": "rsi7"
             },
+            "sma13": false,
+            "ema50": true,
+            "ema100": true,
+            "ema200": true
+        },
+        {
+            "trendPrChannel": {
+                "regression": "trendPrChannel300_2"
+            },
+            "prChannel": {
+                "lower": "prChannelLower200_3_19",
+                "upper": "prChannelUpper200_3_19"
+            },
+            "rsi": {
+                "oversold": 23,
+                "overbought": 77,
+                "rsi": "rsi7"
+            },
+            "sma13": false,
+            "ema50": false,
+            "ema100": false,
+            "ema200": false
+        },
+        {
+            "trendPrChannel": {
+                "regression": "trendPrChannel700_2"
+            },
+            "prChannel": {
+                "lower": "prChannelLower200_2_21",
+                "upper": "prChannelUpper200_2_21"
+            },
+            "rsi": {
+                "oversold": 20,
+                "overbought": 80,
+                "rsi": "rsi7"
+            },
+            "sma13": false,
+            "ema50": false,
+            "ema100": true,
+            "ema200": true
+        },
+        {
+            "trendPrChannel": {
+                "regression": "trendPrChannel500_2"
+            },
+            "prChannel": {
+                "lower": "prChannelLower100_3_195",
+                "upper": "prChannelUpper100_3_195"
+            },
+            "rsi": {
+                "oversold": 20,
+                "overbought": 80,
+                "rsi": "rsi5"
+            },
+            "sma13": true,
+            "ema50": true,
+            "ema100": true,
+            "ema200": true
+        },
+        {
+            "trendPrChannel": {
+                "regression": "trendPrChannel850_2"
+            },
+            "prChannel": {
+                "lower": "prChannelLower200_3_19",
+                "upper": "prChannelUpper200_3_19"
+            },
+            "rsi": {
+                "oversold": 20,
+                "overbought": 80,
+                "rsi": "rsi7"
+            },
+            "sma13": false,
+            "ema50": true,
+            "ema100": true,
+            "ema200": false
+        },
+        {
+            "trendPrChannel": {
+                "regression": "trendPrChannel800_2"
+            },
+            "prChannel": {
+                "lower": "prChannelLower200_3_195",
+                "upper": "prChannelUpper200_3_195"
+            },
+            "rsi": {
+                "oversold": 20,
+                "overbought": 80,
+                "rsi": "rsi7"
+            },
+            "sma13": false,
+            "ema50": true,
+            "ema100": true,
+            "ema200": false
+        },
+        {
+            "trendPrChannel": {
+                "regression": "trendPrChannel650_2"
+            },
+            "prChannel": {
+                "lower": "prChannelLower200_3_19",
+                "upper": "prChannelUpper200_3_19"
+            },
+            "rsi": {
+                "oversold": 23,
+                "overbought": 77,
+                "rsi": "rsi7"
+            },
             "sma13": true,
             "ema50": true,
             "ema100": false,
             "ema200": false
+        },
+        {
+            "trendPrChannel": {
+                "regression": "trendPrChannel300_2"
+            },
+            "prChannel": {
+                "lower": "prChannelLower100_2_20",
+                "upper": "prChannelUpper100_2_20"
+            },
+            "rsi": {
+                "oversold": 23,
+                "overbought": 77,
+                "rsi": "rsi7"
+            },
+            "sma13": false,
+            "ema50": true,
+            "ema100": true,
+            "ema200": true
         }
     ],
     studies: [
@@ -251,14 +273,6 @@ module.exports = {
         },{
             study: studies.Rsi,
             inputs: {
-                length: 2
-            },
-            outputMap: {
-                rsi: 'rsi2'
-            }
-        },{
-            study: studies.Rsi,
-            inputs: {
                 length: 5
             },
             outputMap: {
@@ -274,18 +288,18 @@ module.exports = {
             }
         },
         {study: studies.PolynomialRegressionChannel, inputs: {length: 300, degree: 2}, outputMap: {regression: 'trendPrChannel300_2'}},
-        {study: studies.PolynomialRegressionChannel, inputs: {length: 400, degree: 2}, outputMap: {regression: 'trendPrChannel400_2'}},
-        {study: studies.PolynomialRegressionChannel, inputs: {length: 450, degree: 2}, outputMap: {regression: 'trendPrChannel450_2'}},
+        {study: studies.PolynomialRegressionChannel, inputs: {length: 500, degree: 2}, outputMap: {regression: 'trendPrChannel500_2'}},
+        {study: studies.PolynomialRegressionChannel, inputs: {length: 650, degree: 2}, outputMap: {regression: 'trendPrChannel650_2'}},
         {study: studies.PolynomialRegressionChannel, inputs: {length: 700, degree: 2}, outputMap: {regression: 'trendPrChannel700_2'}},
-        {study: studies.PolynomialRegressionChannel, inputs: {length: 100, degree: 2, deviations: 1.9}, outputMap: {regression: 'prChannel100_2_19', upper: 'prChannelUpper100_2_19', lower: 'prChannelLower100_2_19'}},
-        {study: studies.PolynomialRegressionChannel, inputs: {length: 100, degree: 2, deviations: 2.1}, outputMap: {regression: 'prChannel100_2_21', upper: 'prChannelUpper100_2_21', lower: 'prChannelLower100_2_21'}},
-        {study: studies.PolynomialRegressionChannel, inputs: {length: 200, degree: 2, deviations: 1.95}, outputMap: {regression: 'prChannel200_2_195', upper: 'prChannelUpper200_2_195', lower: 'prChannelLower200_2_195'}},
+        {study: studies.PolynomialRegressionChannel, inputs: {length: 750, degree: 2}, outputMap: {regression: 'trendPrChannel750_2'}},
+        {study: studies.PolynomialRegressionChannel, inputs: {length: 800, degree: 2}, outputMap: {regression: 'trendPrChannel800_2'}},
+        {study: studies.PolynomialRegressionChannel, inputs: {length: 850, degree: 2}, outputMap: {regression: 'trendPrChannel850_2'}},
+        {study: studies.PolynomialRegressionChannel, inputs: {length: 100, degree: 2, deviations: 2.0}, outputMap: {regression: 'prChannel100_2_20', upper: 'prChannelUpper100_2_20', lower: 'prChannelLower100_2_20'}},
+        {study: studies.PolynomialRegressionChannel, inputs: {length: 100, degree: 3, deviations: 1.95}, outputMap: {regression: 'prChannel100_3_195', upper: 'prChannelUpper100_3_195', lower: 'prChannelLower100_3_195'}},
+        {study: studies.PolynomialRegressionChannel, inputs: {length: 100, degree: 3, deviations: 2.15}, outputMap: {regression: 'prChannel100_3_215', upper: 'prChannelUpper100_3_215', lower: 'prChannelLower100_3_215'}},
         {study: studies.PolynomialRegressionChannel, inputs: {length: 200, degree: 2, deviations: 2.0}, outputMap: {regression: 'prChannel200_2_20', upper: 'prChannelUpper200_2_20', lower: 'prChannelLower200_2_20'}},
-        {study: studies.PolynomialRegressionChannel, inputs: {length: 200, degree: 3, deviations: 1.9}, outputMap: {regression: 'prChannel200_3_19', upper: 'prChannelUpper200_3_19', lower: 'prChannelLower200_3_19'}},
-        {study: studies.PolynomialRegressionChannel, inputs: {length: 200, degree: 3, deviations: 2.15}, outputMap: {regression: 'prChannel200_3_215', upper: 'prChannelUpper200_3_215', lower: 'prChannelLower200_3_215'}},
-        {study: studies.PolynomialRegressionChannel, inputs: {length: 250, degree: 4, deviations: 1.9}, outputMap: {regression: 'prChannel250_4_19', upper: 'prChannelUpper250_4_19', lower: 'prChannelLower250_4_19'}},
-        {study: studies.PolynomialRegressionChannel, inputs: {length: 250, degree: 4, deviations: 2.0}, outputMap: {regression: 'prChannel250_4_20', upper: 'prChannelUpper250_4_20', lower: 'prChannelLower250_4_20'}},
-        {study: studies.PolynomialRegressionChannel, inputs: {length: 300, degree: 2, deviations: 2.15}, outputMap: {regression: 'prChannel300_2_215', upper: 'prChannelUpper300_2_215', lower: 'prChannelLower300_2_215'}},
-        {study: studies.PolynomialRegressionChannel, inputs: {length: 300, degree: 2, deviations: 2.1}, outputMap: {regression: 'prChannel300_2_21', upper: 'prChannelUpper300_2_21', lower: 'prChannelLower300_2_21'}}
+        {study: studies.PolynomialRegressionChannel, inputs: {length: 200, degree: 2, deviations: 2.1}, outputMap: {regression: 'prChannel200_2_21', upper: 'prChannelUpper200_2_21', lower: 'prChannelLower200_2_21'}},
+        {study: studies.PolynomialRegressionChannel, inputs: {length: 200, degree: 3, deviations: 1.95}, outputMap: {regression: 'prChannel200_3_195', upper: 'prChannelUpper200_3_195', lower: 'prChannelLower200_3_195'}},
+        {study: studies.PolynomialRegressionChannel, inputs: {length: 200, degree: 3, deviations: 1.9}, outputMap: {regression: 'prChannel200_3_19', upper: 'prChannelUpper200_3_19', lower: 'prChannelLower200_3_19'}}
     ]
 };
