f71f8e245f0784a576bddf29b757ea3d29b90d2a
Chad Johnson <chad.d.johnson@gmail.com>
2016-01-13 10:54:47 -0600

    Only tracking second 59 at server level.

--- src/server/app.js
+++ src/server/app.js
@@ -17,7 +17,7 @@ var messageTypes = {
 // Settings
 var symbols = ['AUDCAD', 'AUDJPY', 'AUDNZD', 'AUDUSD', 'CADJPY', 'EURGBP', 'EURUSD', 'GBPJPY', 'NZDUSD', 'USDCAD', 'USDCHF', 'USDJPY'];
 var tradableSymbols = ['AUDJPY', 'AUDCAD', 'USDJPY'];
-var seconds = [57, 58, 59];  // [56, 57, 58, 59, 0];
+var seconds = [59];  // [56, 57, 58, 59, 0];
 var strategyFn = strategies.Reversals;
 
 var disconnectedAtTimestamp = 0;
